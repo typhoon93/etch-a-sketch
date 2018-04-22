e21286b1f3b99258254333daae8a4fd7e205a28a
typhoon93 <30848523+typhoon93@users.noreply.github.com>
2018-04-23 01:53:00 +0300

    Fixed CSS style to be applicable for all sizes (added grid style), added basic JS function for sketchpad

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-var board = document.querySelector(".board");
+let board = document.querySelector(".board");
+let gridResize = "auto ".repeat(16);
+
+board.setAttribute("style", `grid-template-columns: ${gridResize}; grid-template-rows: ${gridResize};`);
 
 for (let x = 0; x < 256; x++){
     var sketchCell = document.createElement('div');
     sketchCell.className = "sketchcell";
     board.appendChild(sketchCell);
 }
+
+let allSketchCells = Array.from(document.querySelectorAll(".sketchCell"));
+allSketchCells.forEach(cell=>cell.addEventListener('mouseover', (e)=>{
+    if(cell.className == "sketchcellHovered")
+    {
+        cell.className ="sketchcell";
+    } else {
+        cell.className = "sketchcellHovered"
+    }
+}))
+
