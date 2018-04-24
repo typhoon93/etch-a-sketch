ac02a1b301cad6f328317392b751fabe88626032
typhoon93 <30848523+typhoon93@users.noreply.github.com>
2018-04-24 03:42:43 +0300

    Added option to resize canvas, turned all code to separate functions

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
+let columnsAndRows;
 let board = document.querySelector(".board");
-let gridResize = "auto ".repeat(16);
 
-board.setAttribute("style", `grid-template-columns: ${gridResize}; grid-template-rows: ${gridResize};`);
+newSketchPad();
 
-for (let x = 0; x < 256; x++){
+function newSketchPad(){
+    if (!columnsAndRows){
+        columnsAndRows=16;
+        GridResize();   
+        for (let x = 0; x < columnsAndRows*columnsAndRows; x++){
+            createDivs();
+        }
+        changeClassOnHover();
+    } else {
+        columnsAndRows = cellNumberPrompt();
+        GridResize();
+        board.innerHTML=""; // removes initial divs
+        for (let x = 0; x < columnsAndRows*columnsAndRows; x++){
+            createDivs();
+        }
+        changeClassOnHover();
+    }
+}
+
+function cellNumberPrompt(){
+    return prompt("How many rows and columns should your sketchpad have?",)
+}
+function GridResize(){
+    let gridSize = "auto ".repeat(columnsAndRows);
+    board.setAttribute("style", `grid-template-columns: ${gridSize}; grid-template-rows: ${gridSize};`); //resizes grid to desired size
+}
+function createDivs(){
     var sketchCell = document.createElement('div');
     sketchCell.className = "sketchcell";
     board.appendChild(sketchCell);
+    
 }
 
-let allSketchCells = Array.from(document.querySelectorAll(".sketchCell"));
-allSketchCells.forEach(cell=>cell.addEventListener('mouseover', (e)=>{
-    if(cell.className == "sketchcellHovered")
-    {
-        cell.className ="sketchcell";
-    } else {
-        cell.className = "sketchcellHovered"
-    }
+function changeClassOnHover(){
+    const allSketchCells = document.querySelectorAll(".sketchCell");
+allSketchCells.forEach(cell=>cell.addEventListener('mouseover',(e)=>{
+    cell.className = "sketchcellHovered";
 }))
-
+}
